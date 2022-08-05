d5d3875ca100373f4d619a096f16f7afab9c6844
arsiihere <arsiiahmi@gmail.com>
2022-08-05 16:48:56 +0500

    defining some routes

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+app.use("/images", express.static(path.join(__dirname, "./images")));
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error, "this is running");
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({
+    messgae: message,
+  });
+});
+
+mongoose
+  .connect("mongodb+srv://arslan:arslan@cluster0.05v1x.mongodb.net/post")
+  .then((result) => app.listen(8080))
+  .catch((err) => console.log(err, "app is not started"));
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,71 @@
+const { validationResult } = require("express-validator/check");
+const Post = require("../models/Post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: "Fetched Post",
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 404;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Can not find the post");
+        err.statusCode = 404;
+        throw err;
+      }
+      res.status(200).json({
+        message: "Post Fetched",
+        post: post,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 404;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+    next(error);
+  }
+  const { title, content } = req.body;
   // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+  const post = new Post({
+    title: title,
+    content: content,
+    creator: { name: "Arslan Ahmed Arif" },
+    imageUrl: "hell no",
   });
+  post
+    .save()
+    .then((post) => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: post,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
